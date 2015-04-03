87d748cb4ab7ad89f38a07d63a26315afc8e0637
Jim Chiang <jchiang@slac.stanford.edu>
2015-04-02 18:30:08 -0700

    add EdgeRolloffWarper class; refactor generate_star_grid.py

---
+++ python/EdgeRolloffWarper.py
@@ -0,0 +1,66 @@
+import sys
+import numpy as np
+import galsim
+import lsst.afw.image as afwImage
+import lsst.afw.geom as afwGeom
+import lsst.obs.lsstSim as obs_lsstSim
+
+class EdgeRolloffWarper(object):
+    def __init__(self, xamp, xscale, xwidth, yamp, yscale, ywidth):
+        xfunctor = obs_lsstSim.EdgeRolloffFunctor(xamp, xscale, xwidth)
+        yfunctor = obs_lsstSim.EdgeRolloffFunctor(yamp, yscale, ywidth)
+        self.transform = afwGeom.SeparableXYTransform(xfunctor, yfunctor)
+    def jacobian(self, x, y):
+        return (self.transform.getXfunctor().derivative(x)**2 +
+                self.transform.getYfunctor().derivative(y)**2)/2.
+    def run(self, image, verbose=True):
+        try:
+            # Assume it's an afwImage Image.
+            warped_image = image.Factory(image, image.getBBox())
+            wiarr = warped_image.getArray()
+            imarr = image.getArray()
+        except AttributeError:
+            # It must be a galsim Image.
+            warped_image = image.copy()
+            wiarr = warped_image.array
+            imarr = image.array
+        ny, nx = wiarr.shape
+        for xw in range(nx):
+            self._progress(xw, nx, verbose=verbose)
+            for yw in range(ny):
+                ptw = afwGeom.Point2D(xw, yw)
+                pt0 = self.transform.reverseTransform(ptw)
+                x0 = int(np.round(pt0.getX()))
+                y0 = int(np.round(pt0.getY()))
+                try:
+                    wiarr[yw][xw] = imarr[y0][x0]/self.jacobian(pt0.getX(), pt0.getY())
+                except IndexError:
+                    pass
+        if verbose:
+            print "Done"
+        return warped_image
+    def _progress(self, x, nx, verbose=False, dx0=4, dx1=20):
+        if not verbose:
+            return
+        if x % (nx/dx0) == 0:
+            sys.stdout.write('!')
+        elif x % (nx/dx1) == 0:
+            sys.stdout.write('.')
+        sys.stdout.flush()
+
+if __name__ == '__main__':
+    nx, ny = 400, 400
+    #
+    # Create an EdgeRolloffWarper
+    #
+    warper = EdgeRolloffWarper(2, 3, nx, 2, 3, ny)
+    #
+    # Apply it to a flat galsim image
+    #
+    image = galsim.ImageF(nx, ny)
+    image.array[:] += np.ones(image.array.shape)
+
+    warped_image = warper.run(image)
+
+    image.write('unwarped_flat.fits')
+    warped_image.write('warped_flat.fits')
---
+++ python/generate_star_grid.py
@@ -0,0 +1,88 @@
+import sys
+import numpy as np
+import galsim
+from EdgeRolloffWarper import EdgeRolloffWarper
+
+random_seed = 147918741
+
+nxpix = 400      # image size
+nypix = 400
+
+nx = 20          # number of star columns
+ny = 20          # number of star rows
+dx = nxpix/nx    # grid spacing
+dy = nypix/ny
+
+# Warping parameters in pixels
+warp_amp = 2     # Amplitude of astrometric shift.
+warp_scale = 30  # Exponential scale of shift.  This is about a 
+                 # factor of 10 larger than measured
+
+pixel_scale = 1.0    # arcsec / pixel
+sky_level = 1e6      # ADU / arcsec^2
+
+signal_to_noise = 10000   # bright stars
+
+psf_flux = 1
+psf_beta = 3
+psf_fwhm = 2.85
+psf_trunc = 2.*psf_fwhm
+psf_image = galsim.ImageF(nxpix, nypix, scale=pixel_scale)
+
+def offset(x, y, x0=nxpix/2., y0=nypix/2.):
+    return x - x0, y - y0
+
+#
+# Create an array of stars, using a Moffat profile psf.
+#
+print "Creating array of stars"
+psf = galsim.Moffat(beta=psf_beta, fwhm=psf_fwhm, trunc=psf_trunc,
+                    flux=psf_flux)
+for ix in range(nx+1):
+    if ix % ((nx+1)/4) == 0:
+        sys.stdout.write('!')
+        sys.stdout.flush()
+    else:
+        sys.stdout.write('.')
+        sys.stdout.flush()
+    for iy in range(ny+1):
+        xpos = ix*dx - dx/2.
+        ypos = iy*dy - dy/2.
+        psf.drawImage(psf_image, offset=offset(xpos, ypos), add_to_image=True)
+print "Done"
+#
+# Add sky noise.
+#
+ud = galsim.UniformDeviate(random_seed)
+sky_level_pixel = sky_level*pixel_scale**2
+noise = galsim.PoissonNoise(ud, sky_level=sky_level_pixel)
+psf_image.addNoiseSNR(noise, signal_to_noise)
+
+#
+# Warp the image using the edge rolloff transform.  This involves a 
+# pixel-by-pixel mapping
+#
+warper = EdgeRolloffWarper(warp_amp, warp_scale, nxpix,
+                           warp_amp, warp_scale, nypix)
+
+print "Warping image"
+warped_image = warper.run(psf_image)
+
+#
+# Add sky projection TanWCS
+#
+dudx = 1
+dudy = 0
+dvdx = 0
+dvdy = 1
+affine = galsim.AffineTransform(dudx, dudy, dvdx, dvdy,
+                                origin=psf_image.trueCenter())
+sky_center = galsim.CelestialCoord(ra=19.3*galsim.hours,
+                                   dec=-33.1*galsim.degrees)
+wcs = galsim.TanWCS(affine, sky_center, units=galsim.arcsec)
+
+psf_image.wcs = wcs
+warped_image.wcs = wcs
+
+psf_image.write('star_grid_unwarped.fits')
+warped_image.write('star_grid_warped.fits')
