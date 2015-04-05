cc5c7f91e1ee7bb102f3c6f5a7f0c96cb5b9faaf
Jim Chiang <jchiang@slac.stanford.edu>
2015-04-04 22:29:02 -0700

    refactor WCS generation code

--- python/generate_star_grid.py
+++ python/generate_star_grid.py
@@ -3,25 +3,38 @@ import numpy as np
 import galsim
 from EdgeRolloffWarper import EdgeRolloffWarper
 
+def getTanWCS(image, ra=10, dec=0):
+    dudx = 1
+    dudy = 0
+    dvdx = 0
+    dvdy = 1
+    affine = galsim.AffineTransform(dudx, dudy, dvdx, dvdy,
+                                    origin=image.trueCenter())
+    sky_center = galsim.CelestialCoord(ra=ra*galsim.hours,
+                                       dec=dec*galsim.degrees)
+    wcs = galsim.TanWCS(affine, sky_center, units=galsim.arcsec)
+    return wcs
+
 random_seed = 147918741
 
-nxpix = 400      # image size
-nypix = 400
+nxpix = 80       # image size
+nypix = 80
 
-nx = 20          # number of star columns
-ny = 20          # number of star rows
+nx = 4           # number of star columns
+ny = 4           # number of star rows
 dx = nxpix/nx    # grid spacing
 dy = nypix/ny
 
 # Warping parameters in pixels
-warp_amp = 2     # Amplitude of astrometric shift.
-warp_scale = 30  # Exponential scale of shift.  This is about a 
-                 # factor of 10 larger than measured
+warp_amp = 2       # Amplitude of astrometric shift.
+warp_scale = 30    # Exponential scale of shift.  This is about a 
+                   # factor of 10 larger than measured
+warp_width = 4000  # Sensor width
 
 pixel_scale = 0.2    # arcsec / pixel
 sky_level = 1e6      # ADU / arcsec^2
 
-signal_to_noise = 10000   # bright stars
+signal_to_noise = 100000   # bright stars
 
 psf_flux = 1
 psf_beta = 3
@@ -62,27 +75,14 @@ psf_image.addNoiseSNR(noise, signal_to_noise)
 # Warp the image using the edge rolloff transform.  This involves a 
 # pixel-by-pixel mapping
 #
-warper = EdgeRolloffWarper(warp_amp, warp_scale, nxpix,
-                           warp_amp, warp_scale, nypix)
+warper = EdgeRolloffWarper(warp_amp, warp_scale, warp_width,
+                           warp_amp, warp_scale, warp_width)
 
 print "Warping image"
 warped_image = warper.run(psf_image)
 
-#
-# Add sky projection TanWCS
-#
-dudx = 1
-dudy = 0
-dvdx = 0
-dvdy = 1
-affine = galsim.AffineTransform(dudx, dudy, dvdx, dvdy,
-                                origin=psf_image.trueCenter())
-sky_center = galsim.CelestialCoord(ra=19.3*galsim.hours,
-                                   dec=-33.1*galsim.degrees)
-wcs = galsim.TanWCS(affine, sky_center, units=galsim.arcsec)
-
-psf_image.wcs = wcs
-warped_image.wcs = wcs
+psf_image.wcs = getTanWCS(psf_image)
+warped_image.wcs = psf_image.wcs
 
 psf_image.write('star_grid_unwarped.fits')
 warped_image.write('star_grid_warped.fits')
