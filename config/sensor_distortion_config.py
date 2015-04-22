5815c56d515b90c7f47bdfd879773568679c80c3
Jim Chiang <jchiang@slac.stanford.edu>
2015-04-22 09:13:17 -0700

    use sensorDistortions from this repo instead of obs_lsstSim branch

---
+++ config/sensor_distortion_config.py
@@ -0,0 +1,13 @@
+root.edgeRolloff.xamp = -2
+root.edgeRolloff.xscale = 30
+root.edgeRolloff.xwidth = 4096
+root.edgeRolloff.yamp = -2
+root.edgeRolloff.yscale = 30
+root.edgeRolloff.ywidth = 4004
+root.edgeRolloff.applyModel = True
+
+#root.linearWarp.xslope = 1.
+#root.linearWarp.xintercept = 2.
+#root.linearWarp.yslope = 1.
+#root.linearWarp.yintercept = 3.
+#root.linearWarp.applyModel = True
